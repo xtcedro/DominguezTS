e46a43891419cebc7e02e24abf8c289a7736b246
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-28 00:57:31 -0800

    Update load-components.js

--- public/assets/js/load-components.js
+++ public/assets/js/load-components.js
@@ -1,6 +1,6 @@
 // Load Header
 export function loadHeader() {
-    fetch('../components/header.html')
+    fetch('../../components/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
@@ -10,7 +10,7 @@ export function loadHeader() {
 
 // Load Footer
 export function loadFooter() {
-    fetch('../components/footer.html')
+    fetch('../../components/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
